de6126ca7aa25549a580a99e90e7ef3e029e5a42
Luiz Eduardo Domingues <domingues-pc@hotmail.com>
2023-08-10 16:08:04 -0400

    Docker

--- UserAPI/obj/Debug/net6.0/UserAPI.AssemblyInfo.cs
+++ UserAPI/obj/Debug/net6.0/UserAPI.AssemblyInfo.cs
@@ -11,6 +11,7 @@
 using System;
 using System.Reflection;
 
+[assembly: Microsoft.Extensions.Configuration.UserSecrets.UserSecretsIdAttribute("9339c356-7ad5-4bcc-ad3a-67ab9bef7e81")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("UserAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
