201f8a3a43bb50c43d100ee4018073311c7e03be
hit25082000 <domingues-pc@hotmail.com>
2024-06-23 13:57:47 -0400

    xd

---
+++ UserAPI/Models/HtmlTemplate.cs
@@ -0,0 +1,14 @@
+﻿namespace UserAPI.Models
+{
+    public class HtmlTemplate : Entity
+    {
+        public HtmlTemplate()
+        {
+            Carrossel = new Noticia[3];
+        }
+
+        public Noticia Home { get; set; }
+        public Noticia[] Carrossel { get; set; }
+        public List<Aula> Videos { get; set; }
+    }
+}
